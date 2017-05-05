52669a88ad843b323461599a24bf660b023b54c6
xoming <xoming@ubuntu.(none)>
2017-05-05 16:01:18 +0800

    add ts qm files

---
+++ 04_lang/lang_en.ts
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished">MainWindow_en</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="27"/>
+        <source>PushButton</source>
+        <translation type="unfinished">PushButton_en</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="40"/>
+        <location filename="mainwindow.ui" line="53"/>
+        <source>TextLabel</source>
+        <translation type="unfinished">TextLabel_en</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="67"/>
+        <source>chinese</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="72"/>
+        <source>english</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ 04_lang/lang_zh.ts
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished">MainWindow_zh</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="27"/>
+        <source>PushButton</source>
+        <translation type="unfinished">PushButton_zh</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="40"/>
+        <location filename="mainwindow.ui" line="53"/>
+        <source>TextLabel</source>
+        <translation type="unfinished">TextLabel_zh</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="67"/>
+        <source>chinese</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="72"/>
+        <source>english</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
